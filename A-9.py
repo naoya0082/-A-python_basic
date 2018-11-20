a542f4fb77c28dff7d32f6deb9bcb5af95fb22b1
naoya0082 <6ds3f7@gmail.com>
2018-11-20 11:59:46 +0900

    A-9 完了 疑問点なし

---
+++ A-9.py
@@ -0,0 +1,6 @@
+# 期待するコードが動作するよう辞書を定義
+kazuma_info = {"first_name": "Kazma", "family_name": "Takahashi", "age": 35}
+
+print(kazuma_info["first_name"])  # "Kazuma"
+print(kazuma_info["family_name"])  # "Takahashi"
+print(kazuma_info["age"])  # 35
